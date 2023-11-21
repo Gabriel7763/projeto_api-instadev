ef082c760f6d4d20acae08a43b2bae0c73aa5c07
Gabriel Nunes <94002497+Gabriel7763@users.noreply.github.com>
2023-11-21 14:37:35 -0300

    create route to update a post

--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,9 @@ routes.delete('/user', UserController.delete);
 routes.get('/user-profile', UserController.userProfile);
 
 routes.post('/upload', upload.single('image'), FileController.upload);
-routes.post('/new-post', schemaValidator(postSchema), PostController.create);
-routes.delete('/delete-post/:id', PostController.delete);
+
+routes.post('/post', schemaValidator(postSchema), PostController.create);
+routes.delete('/post/:id', PostController.delete);
+routes.put('/post/:id', PostController.update);
 
 module.exports = routes;
