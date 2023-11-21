05292fbcb4398d5271588be5c321ad1d19f37be3
Gabriel Nunes <94002497+Gabriel7763@users.noreply.github.com>
2023-11-21 20:59:19 -0300

    refactor no padrão REST e adicionando CORS

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-console */
-/* eslint linebreak-style: ["error", "windows"] */
-
 require('dotenv').config();
 require('./database/index');
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 const { PORT } = process.env;
