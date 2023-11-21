3e0289239c9d96afdcbf5ccaa491b2c9df6295c4
Gabriel Nunes <94002497+Gabriel7763@users.noreply.github.com>
2023-11-21 18:12:41 -0300

    created endpoint to list all posts

--- src/routes.js
+++ src/routes.js
@@ -38,5 +38,6 @@ routes.delete('/post/:id', PostController.delete);
 routes.put('/post/:id', PostController.update);
 routes.put('/add-like/:id', PostController.addLike);
 routes.get('/list-my-posts/', PostController.listMyPosts);
+routes.get('/list-all-posts/', PostController.listAllPosts);
 
 module.exports = routes;
